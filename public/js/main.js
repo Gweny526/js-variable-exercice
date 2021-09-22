f50f1af031b9c9f99a9916dc075cca5d2d90240a
Gweny <gweny.ds@gmail.com>
2021-09-22 16:20:40 +0200

    exo5 finish

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+// exercice 1
+let number1 = 5;
+let number2 = 25;
+let resultat = number1 + number2;
+console.log(resultat);
+// ` number1 + number2 = resultat `
+
+// excercice 2
+let age = prompt("Please enter you age")
+let firstName = prompt("Please enter your first name");
+let sentence = firstName + " " + "a" + " " + age + "ans cette année";
+console.log(sentence);
+
+// exercice 3
+let birthdayYears = 1991;
+let yearNow = 2021;
+let age2 = yearNow - birthdayYears;
+console.log(age2);
+let newYear = 2023;
+let age3 = newYear - birthdayYears;
+console.log(age3);
+
