c584218935a9924a2766145bd14ae8ec923d3e12
Gweny <gweny.ds@gmail.com>
2021-10-16 13:41:38 +0200

    exo variable 5

--- public/js/main.js
+++ public/js/main.js
@@ -3,20 +3,33 @@ let number1 = 5;
 let number2 = 25;
 let resultat = number1 + number2;
 console.log(resultat);
+// verifier la correction
+console.log(` ${number1} + ${number2} = ${number1 + number2} `);
+console.log(` ${number1} - ${number2} = ${number1 - number2} `);
+console.log(` ${number1} / ${number2} = ${number1 / number2} `);
+console.log(` ${number1} * ${number2} = ${number1 * number2} `);
 // ` number1 + number2 = resultat `
 
-// excercice 2
-let age = prompt("Please enter you age")
-let firstName = prompt("Please enter your first name");
-let sentence = firstName + " " + "a" + " " + age + "ans cette année";
-console.log(sentence);
-
-// exercice 3
+// // excercice 2
+// let age = prompt("Please enter you age")
+// let firstName = prompt("Please enter your first name");
+// let sentence = firstName + " " + "a" + " " + age + "ans cette année";
+// console.log(sentence);
+// // correction
+// console.log(`${firstName} a ${age} ans cette année`);
+// console.log(`${firstName} a ${age+3} ans dans 3 ans`);
+// correction
+// let age = prompt(`enter your age`);
+// let numberConvert = parseInt(age);
+// console.log(typeof numberConvert);
+// ou sur une ligne de code
+// let age = parseInt(prompt('enter your age'));
+// // exercice 3
 let birthdayYears = 1991;
 let yearNow = 2021;
-let age2 = yearNow - birthdayYears;
-console.log(age2);
+// let age2 = yearNow - birthdayYears;
+console.log(yearNow - birthdayYears);
 let newYear = 2023;
-let age3 = newYear - birthdayYears;
-console.log(age3);
+// let age3 = newYear - birthdayYears;
+console.log((yearNow - birthdayYears)+2);
 
